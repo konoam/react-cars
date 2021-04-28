cd1fc8b4a6182e2508aeb11f75a78bde1a04f25a
skarlinski <skarlinski@gmail.com>
2021-04-28 10:40:13 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-
+import CarsView from './components/CarsView';
+import Car from './data-models/Car';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// ./ means the current directory
+// ../ means go up one directory
+// ./components/CarsView
 function App() {
+  const car1 = new Car('Mazda', '6', 2017, 10000);
+  const car2 = new Car('Bentley', 'T14', 2019, 20000);
+  const car3 = new Car('Audi', 'tt', 2018, 15000);  
+  const carsArray = [car1, car2, car3];
   return (
     <div>
-
+      <CarsView carsData={carsArray}></CarsView>
     </div>
   );
 }
