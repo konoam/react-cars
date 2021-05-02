378cd7bb9ba42491eedd7aeb6a9f045f3ec479a1
skarlinski <skarlinski@gmail.com>
2021-05-02 13:03:25 +0300

    - Created json for cars data - Made App.js class syntax

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
+import React from 'react';
+
 import './App.css';
 import CarsView from './components/CarsView';
-import Car from './data-models/Car';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // ./ means the current directory
 // ../ means go up one directory
 // ./components/CarsView
-function App() {
-  const car1 = new Car('Mazda', '6', 2017, 10000, 'dfasdf-123-3');
-  const car2 = new Car('Bentley', 'T14', 2019, 20000, 'dfdf-32253-3');
-  const car3 = new Car('Audi', 'tt', 2018, 15000, 'qqqq-32253-3');  
-  const carsArray = [car1, car2, car3];
-  return (
-    <div>
-      <CarsView carsData={carsArray} />
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <CarsView carsData={[]}/>
+      </div>
+    );    
+  }
+
 }
 
 export default App;
