95ecc4d60a0b8c41bd0325623c6bdf5ada2b83ef
skarlinski <skarlinski@gmail.com>
2021-05-02 13:49:15 +0300

    prepare task to send ajax data

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // ../ means go up one directory
 // ./components/CarsView
 class App extends React.Component {
+  // 1) After loading the page, get the data from cars-data.json
+  // 2) Convert data to Car class
+  // 3) Pass the cars into the carsData prop
+  // Where should I put the AJAX call?
+  constructor(props){
+    super(props);
+    // 1) Create a componentDidMount Hook
+    // 2) Save the json data into a variable
+    // 3) Save the json data into state
+    // 4) use State to pass to the carsData prop.
+  }
+
   render(){
     return (
       <div>
